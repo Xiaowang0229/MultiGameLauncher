07c67ff162271f477f18bd1c0bcde3123cd982f5
Xiaowang0229 <wangjiaxu20120229@163.com>
2025-12-20 10:33:01 +0800

    Update Versioning Specification

--- Tools.cs
+++ Tools.cs
@@ -22,7 +22,7 @@ namespace MultiGameLauncher
 {
     public class Variables //变量集
     {
-        public readonly static string Version = "Indev 251214\n";
+        public readonly static string Version = "Beta 0.101.8.1\n";
         public readonly static string Configpath = Environment.CurrentDirectory + @"\Config.json";
         public readonly static string VersionLog = $"#[{Version.Substring(0, Version.Length - 1)} 版本日志]\r\n-1.xxx\r\n-2.xxx\r\n-3.xxx";
     }
